6da67d5b504666dd7305a30a22f8c90a65094c55
Padma sree Gunturi <padmasree282003@gmail.com>
2022-12-21 05:51:10 +0530

    level 2 http server

--- index.js
+++ index.js
@@ -1,2 +1,60 @@
-let hello= '"Hello Node.js!"';
-console.log(hello);
+const http = require("http");
+const fs = require("fs");
+
+const arg = require("minimist")(process.argv.slice(2));
+console.log(arg);
+const port = arg._[0];
+console.log(port);
+
+
+let homeContent = "";
+let projectContent ="";
+let registrationContent ="";
+
+
+fs.readFile("home.html" , (err,home) =>{
+    if(err){
+        throw err;
+
+    }
+    homeContent = home;
+});
+
+fs.readFile("projects.html" , (err, data) =>{
+    if(err){
+        throw err;
+
+    }
+    projectContent = data;
+});
+
+fs.readFile("registration.html" , (err,data) =>{
+    if(err){
+        throw err;
+
+    }
+    registrationContent = data;
+});
+
+http
+ .createServer((req,res) => {
+    let url = req.url;
+    res.writeHeader(200,{ "Content-Type":"text/html"});
+    switch(url){
+        case "/projects":
+            res.write(projectContent);
+            res.end();
+            break;
+        case "/registration":
+            res.write(registrationContent);
+            res.end();
+            break;
+        default:
+            res.write(homeContent);
+            res.end();
+            break;
+    }
+ })
+ .listen(port ,() =>{
+    console.log(`Server is listening at ${port}`);
+ });
