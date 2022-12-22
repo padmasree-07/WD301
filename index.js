a9f3d9743d3814397df33f104f89387426938e85
Padma sree Gunturi <padmasree282003@gmail.com>
2022-12-22 11:52:27 +0530

    Todo application using Closures

--- index.js
+++ index.js
@@ -1,60 +1,117 @@
-const http = require("http");
-const fs = require("fs");
-
-const arg = require("minimist")(process.argv.slice(2));
-console.log(arg);
-const port = arg._[0];
-console.log(port);
-
-
-let homeContent = "";
-let projectContent ="";
-let registrationContent ="";
-
-
-fs.readFile("home.html" , (err,home) =>{
-    if(err){
-        throw err;
-
+const todoList = () => {
+    all = []
+    const add = (todoItem) => {
+      all.push(todoItem)
     }
-    homeContent = home;
-});
-
-fs.readFile("projects.html" , (err, data) =>{
-    if(err){
-        throw err;
-
+    const markAsComplete = (index) => {
+      all[index].completed = true
     }
-    projectContent = data;
-});
-
-fs.readFile("registration.html" , (err,data) =>{
-    if(err){
-        throw err;
-
+  
+    const overdue = () => {
+      let ar=[]
+      let l = all.length;
+      for(let i=0 ; i<l ; i++){
+          if (all[i].dueDate < today){
+            ar.push(all[i]);
+          }
+      }
+      return ar
     }
-    registrationContent = data;
-});
-
-http
- .createServer((req,res) => {
-    let url = req.url;
-    res.writeHeader(200,{ "Content-Type":"text/html"});
-    switch(url){
-        case "/projects":
-            res.write(projectContent);
-            res.end();
-            break;
-        case "/registration":
-            res.write(registrationContent);
-            res.end();
-            break;
-        default:
-            res.write(homeContent);
-            res.end();
-            break;
+  
+    const dueToday = () => {
+      // Write the date check condition here and return the array of todo items that are due today accordingly.
+      // FILL YOUR CODE HERE
+      let ar=[]
+      let l = all.length;
+      for(let i=0 ; i<l ; i++){
+          if (all[i].dueDate == today){
+            ar.push(all[i]);
+          }
+      }
+      return ar
+       
     }
- })
- .listen(port ,() =>{
-    console.log(`Server is listening at ${port}`);
- });
+  
+    const dueLater = () => {
+      // Write the date check condition here and return the array of todo items that are due later accordingly.
+      // FILL YOUR CODE HERE
+      let ar=[]
+      let l = all.length;
+      for(let i=0 ; i<l ; i++){
+          if (all[i].dueDate >today){
+            ar.push(all[i]);
+          }
+      }
+      return ar
+    }
+  
+    const toDisplayableList = (list) => {
+      // Format the To-Do list here, and return the output string as per the format given above.
+      // FILL YOUR CODE HERE
+      let st="";
+      for(let i=0;i<list.length;i++){
+         if (list[i].completed == true){
+            st = st+ "[x] ";
+         }
+         else{
+            st =st+ "[] ";
+         }
+         st = st+ list[i].title;
+         st = st +" ";
+         if (list[i].dueDate !=today){
+           st = st+list[i].dueDate;
+
+         }
+        st = st+"\n";
+      }
+      return st;
+      // return OUTPUT_STRING
+    }
+  
+    return { all, add, markAsComplete, overdue, dueToday, dueLater, toDisplayableList };
+  }
+  
+  // ####################################### #
+  // DO NOT CHANGE ANYTHING BELOW THIS LINE. #
+  // ####################################### #
+  
+  const todos = todoList();
+  
+  const formattedDate = d => {
+    return d.toISOString().split("T")[0]
+  }
+  
+  var dateToday = new Date()
+  const today = formattedDate(dateToday)
+  const yesterday = formattedDate(
+    new Date(new Date().setDate(dateToday.getDate() - 1))
+  )
+  const tomorrow = formattedDate(
+    new Date(new Date().setDate(dateToday.getDate() + 1))
+  )
+  
+  todos.add({ title: 'Submit assignment', dueDate: yesterday, completed: false })
+  todos.add({ title: 'Pay rent', dueDate: today, completed: true })
+  todos.add({ title: 'Service Vehicle', dueDate: today, completed: false })
+  todos.add({ title: 'File taxes', dueDate: tomorrow, completed: false })
+  todos.add({ title: 'Pay electric bill', dueDate: tomorrow, completed: false })
+  
+  console.log("My Todo-list\n\n")
+  
+  console.log("Overdue")
+  var overdues = todos.overdue()
+  var formattedOverdues = todos.toDisplayableList(overdues)
+  console.log(formattedOverdues)
+  console.log("\n\n")
+  
+  console.log("Due Today")
+  let itemsDueToday = todos.dueToday()
+  let formattedItemsDueToday = todos.toDisplayableList(itemsDueToday)
+  console.log(formattedItemsDueToday)
+  console.log("\n\n")
+  
+  console.log("Due Later")
+  let itemsDueLater = todos.dueLater()
+  let formattedItemsDueLater = todos.toDisplayableList(itemsDueLater)
+  console.log(formattedItemsDueLater)
+  console.log("\n\n")
